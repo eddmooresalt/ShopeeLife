4e891334620506bef310251bcab86a012f6b0092
eddmooresalt <secretmack@hotmail.com>
2025-06-07 23:02:53 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
-// app/layout.tsx
-import './globals.css';
-import React from 'react';
-import type { Metadata } from 'next';
+import '../styles/globals.css';
 
-export const metadata: Metadata = {
-  title: 'ShopeeLife App',
-  description: 'ShopeeLife web app',
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />

7b70ac231f127c1ea39349f4b5b7958922f451bd
eddmooresalt <secretmack@hotmail.com>
2025-06-07 22:51:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
 // app/layout.tsx
+import './globals.css';
+import React from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ShopeeLife App',
+  description: 'ShopeeLife app deployed via Vercel.',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">

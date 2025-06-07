4d649ecc1d85221f1cfb500efa045497d6afabf9
eddmooresalt <secretmack@hotmail.com>
2025-06-07 23:17:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 // app/layout.tsx
 import '../styles/globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
